d9ecfc4541f1d96663a2b6763722bf42307cff23
xhonatankonomi <xhoni8962@gmail.com>
2025-10-20 16:10:36 +0200

    i commenti sono note che ho inserito io per me stesso

--- assets/script/main.js
+++ assets/script/main.js
@@ -38,31 +38,47 @@ const teamMembers = [
 ];
 
 
+// Seleziona l'elemento HTML con l'ID 'gertrudo' e lo memorizza nella variabile 'gertrudo'.
+// Questo elemento è il contenitore dove verrà visualizzato il contenuto dinamico.
 const gertrudo = document.getElementById('gertrudo');
 
+// Inizializza una stringa vuota. Questa variabile accumulerà l'HTML generato
+// per tutte le card prima di essere inserita nel DOM.
 let cardsHTML = "";
 
+// Avvia un ciclo 'for' per iterare su ogni oggetto (membro del team) presente nell'array 'teamMembers'.
+// Si assume che l'array 'teamMembers' sia definito altrove nel codice e contenga i dati.
 for (let index = 0; index < teamMembers.length; index++) {
+    // Ad ogni iterazione, assegna l'oggetto corrente dell'array alla variabile 'element'.
     const element = teamMembers[index];
+    
+    // Utilizza la "destructuring assignment" per estrarre le proprietà necessarie ('name', 'role', 'email', 'img')
+    // dall'oggetto 'element'. Questo rende l'accesso a questi dati più conciso.
     const {name, role, email, img} = element;
+    
+    // Concatena l'HTML per la card del membro del team corrente alla variabile 'cardsHTML'.
+    // Viene utilizzato un template literal (stringa tra apici rovesci) per includere le variabili.
     cardsHTML += `
         <div class="col-lg-6">
-                    <div class="card">
-                        <div class="row g-0">
-                            <div class="col-md-4">
-                                <img src="./assets/${img}" class="img-fluid rounded-start" alt="Team Member">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">${name}</h5>
-                                    <p class="card-text">${role}</p>
-                                    <p class="card-text">${email}</p>
-                                </div>
-                            </div>
+            <div class="card">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="./assets/${img}" class="img-fluid rounded-start" alt="Team Member">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${name}</h5>
+                            <p class="card-text">${role}</p>
+                            <p class="card-text">${email}</p>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
     `
 }
 
+// Terminato il ciclo, assegna l'intera stringa di HTML accumulata ('cardsHTML')
+// come contenuto interno (innerHTML) dell'elemento selezionato ('gertrudo').
+// Questo passo renderizza tutte le card sulla pagina.
 gertrudo.innerHTML = cardsHTML
